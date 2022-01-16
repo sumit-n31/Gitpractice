906ee3dd3c9946cf966d8643f8fb1e2ebf2dc029
Agnii <sekshinge97@gmail.com>
2022-01-16 16:03:45 +0530

    My first commit

--- src/main/java/config/BaseClass.java
+++ src/main/java/config/BaseClass.java
@@ -2,8 +2,15 @@ package config;
 
 public class BaseClass {
 
+	public void sum() {
+		System.out.println("Sum");
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Gitpractice BaseClass");
+		BaseClass b = new BaseClass();
+		b.sum();
 
 	}
 
